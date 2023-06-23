e9f80cab4dbf47f219608901f4f97e86bb0d60d1
ExecutiveAV <info@executiveav.com>
2023-06-23 16:21:29 -0600

    Revert "Added redux-persist and wrapper-redux-next, persisted redux store"
    This reverts commit d4d45096ae8d2cb821d1fc15e1851750d5a0fd21.
    
    Reverted

--- app/redux/store/store.ts
+++ app/redux/store/store.ts
@@ -1,37 +1,15 @@
-import { configureStore, Store } from '@reduxjs/toolkit';
-import { createWrapper, MakeStore, Context } from 'next-redux-wrapper';
-import { persistStore, persistReducer } from 'redux-persist';
-import { combineReducers } from 'redux';
-
-import storage from 'redux-persist/lib/storage';
-
+import { configureStore } from "@reduxjs/toolkit";
 import scheduleReducer from "../slices/scheduleSlice";
 import newEntryPortalReducer from "../slices/portalSlice";
 import newEntryReducer from "../slices/newEntrySlice";
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  // Expires in 1 day (in milliseconds)
-  expireIn: 24 * 60 * 60 * 1000,
-};
+export const store = configureStore({
+    reducer: {
+        schedule: scheduleReducer,
+        newEntryPortal: newEntryPortalReducer,
+        newEntry: newEntryReducer,
+    },
+});
 
-export const rootReducer = combineReducers({
-    schedule: scheduleReducer,
-    newEntryPortal: newEntryPortalReducer,
-    newEntry: newEntryReducer,
-  });  
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export type RootState = ReturnType<typeof rootReducer>;
-
-export const makeStore: MakeStore<Store<RootState>> = (context: Context) => {
-    const store = configureStore({
-      reducer: persistedReducer,
-    });
-    persistStore(store);
-    return store;
-};  
-
-export const wrapper = createWrapper<Store<RootState>>(makeStore, { debug: true });
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
