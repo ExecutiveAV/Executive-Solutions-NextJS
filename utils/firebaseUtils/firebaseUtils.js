146a328b7e4be6a19e3278a7b7f3df552a00a1c5
ExecutiveAV <info@executiveav.com>
2023-07-29 19:13:20 -0400

    Changed from hidden file to env var for firebaseConfig

--- utils/firebaseUtils/firebaseUtils.js
+++ utils/firebaseUtils/firebaseUtils.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
-import firebaseConfig from '../../firebaseKeys';
+// import firebaseConfig from '../../firebaseKeys';
 import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
 export const firebaseApp = initializeApp(firebaseConfig);
 
